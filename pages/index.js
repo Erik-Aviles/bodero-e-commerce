d172a6332c708401b799aa1f130fadf4d09b77fa
Erika Avilés <eriavicor34@gmail.com>
2024-07-20 15:06:33 -0500

    sin callback

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,41 @@
 import Head from "next/head";
-import { endpointsHome } from "@/resources/endpointsHome";
 import CartDashboard from "@/components/CartDashboard";
-import { dashList } from "@/resources/dashList";
 import Layout from "@/components/Layout";
 import { fetcher } from "@/utils/fetcher";
 import useSWR from "swr";
+import { dashList } from "@/resources/dashList";
 
 export default function Home() {
-  const refreshInterval = 600000;
-
-  const swrData = {};
-  const swrLoadingStates = {};
-
-  endpointsHome.forEach(({ key, url }) => {
-    const { data, isLoading } = useSWR(url, fetcher, { refreshInterval });
-    swrData[key] = data;
-    swrLoadingStates[`isLoad${key.charAt(0).toUpperCase() + key.slice(1)}`] =
-      isLoading;
-  });
+  const { data: sizeProducts, isLoading: isLoadProducts } = useSWR(
+    "/api/products/size",
+    fetcher,
+    { refreshInterval: 600000 }
+  );
+  const { data: sizeCategories, isLoading: isLoadCategories } = useSWR(
+    "/api/categories/size",
+    fetcher,
+    { refreshInterval: 600000 }
+  );
+  const { data: sizeUsers, isLoading: isLoadUsers } = useSWR(
+    "/api/users/size",
+    fetcher,
+    { refreshInterval: 600000 }
+  );
+  const { data: sizeCustomers, isLoading: isLoadCustomers } = useSWR(
+    "/api/customers/size",
+    fetcher,
+    { refreshInterval: 600000 }
+  );
+  const { data: sizeOrders, isLoading: isLoadOrders } = useSWR(
+    "/api/orders/size",
+    fetcher,
+    { refreshInterval: 600000 }
+  );
+  const { data: sizeOrdersList, isLoading: isLoadOrdersList } = useSWR(
+    "/api/orderslist/size",
+    fetcher,
+    { refreshInterval: 600000 }
+  );
 
   return (
     <>
@@ -50,24 +68,20 @@ export default function Home() {
                     src={src}
                     alt={alt}
                     isLoading={
-                      (title === "productos" &&
-                        swrLoadingStates.isLoadSizeProducts) ||
-                      (title === "categorias" &&
-                        swrLoadingStates.isLoadSizeCategories) ||
-                      (href === "/users" && swrLoadingStates.isLoadSizeUsers) ||
-                      (href === "/customers" &&
-                        swrLoadingStates.isLoadSizeCustomers) ||
-                      (href === "/orderslist" &&
-                        swrLoadingStates.isLoadSizeOrdersList) ||
-                      (href === "/orders" && swrLoadingStates.isLoadSizeOrders)
+                      (title === "productos" && isLoadProducts) ||
+                      (title === "categorias" && isLoadCategories) ||
+                      (href === "/users" && isLoadUsers) ||
+                      (href === "/customers" && isLoadCustomers) ||
+                      (href === "/orderslist" && isLoadOrdersList) ||
+                      (href === "/orders" && isLoadOrders)
                     }
                     itemCount={
-                      (title === "productos" && swrData.sizeProducts) ||
-                      (title === "categorias" && swrData.sizeCategories) ||
-                      (href === "/users" && swrData.sizeUsers) ||
-                      (href === "/customers" && swrData.sizeCustomers) ||
-                      (href === "/orderslist" && swrData.sizeOrdersList) ||
-                      (href === "/orders" && swrData.sizeOrders)
+                      (title === "productos" && sizeProducts) ||
+                      (title === "categorias" && sizeCategories) ||
+                      (href === "/users" && sizeUsers) ||
+                      (href === "/customers" && sizeCustomers) ||
+                      (href === "/orderslist" && sizeOrdersList) ||
+                      (href === "/orders" && sizeOrders)
                     }
                   />
                 ))}
