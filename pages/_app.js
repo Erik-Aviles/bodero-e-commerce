2c60c238d6c49a02e8b71524ab449b7ef405aa23
Erika Avilés <eriavicor34@gmail.com>
2024-10-25 11:57:46 -0500

    zoom en imput (viewport)

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { NotificationProvider } from "@/context/NotificationContext";
 import { ActionsProvider } from "@/context/actionsProvider";
 import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({
@@ -10,14 +11,23 @@ export default function App({
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <NextUIProvider>
-      <NotificationProvider>
-        <SessionProvider session={session}>
-          <ActionsProvider>
-            <Component {...pageProps} />
-          </ActionsProvider>
-        </SessionProvider>
-      </NotificationProvider>
-    </NextUIProvider>
+    <>
+      <Head>
+        {/* Metaetiqueta viewport */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <NextUIProvider>
+        <NotificationProvider>
+          <SessionProvider session={session}>
+            <ActionsProvider>
+              <Component {...pageProps} />
+            </ActionsProvider>
+          </SessionProvider>
+        </NotificationProvider>
+      </NextUIProvider>
+    </>
   );
 }
